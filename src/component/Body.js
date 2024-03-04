cb77fa5bcad9b202dbd0d8d263fd033fca0c1b02
Dharam Das <dharamdas111@gmail.com>
2024-03-04 16:35:37 +0530

    Initial Commit

---
+++ src/component/Body.js
@@ -0,0 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./Login";
+import Browser from "./Browse";
+
+const Body =()=>{
+    const appRouter = createBrowserRouter([
+         {
+            path:"/",
+            element: <Login />
+         },{
+            path:'/browse',
+            element:<Browser />
+         }
+    ])
+    return(
+        <div>
+            <RouterProvider router={appRouter} />
+        </div>
+    )
+}
+
+export default Body;
